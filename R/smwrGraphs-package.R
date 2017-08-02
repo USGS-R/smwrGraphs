89536b7d4690cff106543bbd07479302f05e5f57
Laura DeCicco <ldecicco@usgs.gov>
2017-08-02 10:58:04 -0500

    Changing startup message

--- R/smwrGraphs-package.R
+++ R/smwrGraphs-package.R
@@ -133,12 +133,8 @@
 #' 
 NULL
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("This information is preliminary or provisional and
-is subject to revision. It is being provided to meet
-the need for timely best science. The information
-has not received final approval by the U.S. Geological
-Survey (USGS) and is provided on the condition that
-neither the USGS nor the U.S. Government shall be held
-liable for any damages resulting from the authorized
-or unauthorized use of the information.")
+  packageStartupMessage(paste(strwrap(
+    'USGS Support Package: 
+  https://owi.usgs.gov/R/packages.html#support'),
+      collapse='\n'))
 }
